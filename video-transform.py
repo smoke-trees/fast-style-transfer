981637e29b7e817a88cb9661996e1edc8c56aaa1
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-18 12:25:24 +0530

    Transformed images
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- video-transform.py
+++ video-transform.py
@@ -5,6 +5,7 @@ Created on Sat Apr 18 11:43:26 2020
 
 @author: tanmay
 """
+import os
 import cv2
 import utils
 
@@ -12,7 +13,17 @@ import tensorflow as tf
 import tensorflow_hub as hub
 
 
-style_path = 'style.jpeg'
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config = config)
+
+
+style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
+
+hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
 
 style_image = utils.load_img(style_path)
 
@@ -30,3 +41,11 @@ while(cap.isOpened()):
  
 cap.release()
 cv2.destroyAllWindows()
+
+imgs = sorted(os.listdir('images_transformed/'))
+
+for img in imgs:
+    content_image = utils.load_img('images_transformed/' + img)
+    stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+    copy = utils.tensor_to_image(stylized_image)
+    copy.save('images_transformed/transformed_' + img)
